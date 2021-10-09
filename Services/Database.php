3b7cfbae6a07b12bac69072b7eaf8dfa4c838e9a
Aleksandra Mickoska <mickoskaaleksandra@gmail.com>
2021-10-09 20:48:29 +0200

    Database interface created

---
+++ Services/Database.php
@@ -0,0 +1,52 @@
+<?php
+require_once __DIR__ . "../../consts.php";
+require_once __DIR__ . '../../IDatabase.php';
+
+class Database implements IDatabase
+{
+    private $pdo;
+
+    private $_getDoctors="SELECT * FROM doctors";
+
+    private $_checkDoctor="SELECT * FROM doctor WHERE Email_address=:email AND Password=:password";
+
+    function __construct()
+    {
+        $this->initializeDatabase();
+    }
+
+    private function initializeDatabase()
+    {
+        try {
+            $this->pdo= new PDO(
+                "mysql:host=localhost;dbname=" . DB_NAME,DB_USER,DB_PASS,
+                [
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+            );
+        } catch (PDOException $e) {
+            echo "Database connection failed";
+            die();
+        }
+    }
+
+    public function GetDoctors()
+    {
+        $stmt=$this->pdo->prepare($this->_getDoctors);
+        $stmt->execute([]);    
+        return $stmt->fetchAll();
+    }
+
+    public function CheckDoctor($email, $password)
+    {
+        $stmt=$this->pdo->prepare($this->_checkDoctor);
+        $stmt->bindValue('email', $email, PDO::PARAM_STR);
+        $stmt->bindValue('password', $password, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch();
+
+
+    }
+}
+
+?>
