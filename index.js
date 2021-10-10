fcd37439387111eb4525c3084fbd886d613dbae7
Aleksandra Mickoska <mickoskaaleksandra@gmail.com>
2021-10-10 13:51:18 +0200

    Reloading problem solved

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-    function Login()
-    {
-        let emailValue = document.getElementById("email").value;
-        let passwordValue = document.getElementById("password").value;
+$(document).ready(function(){
 
-        let data=new FormData();
-        data.append("method","Login");
-        data.append("email",emailValue);
-        data.append("password",passwordValue);
+});
 
-        let xtmlRequest=new XMLHttpRequest();
-        xtmlRequest.open("POST","Services/Service.php");
-        xtmlRequest.onload= function()
-        {
-            //
-        };
-        xtmlRequest.send(data);
-        console.log('Bidna');
-        return false;
-    }
-        
-        
+function Login()
+{
+    let emailValue = document.getElementById("email").value;
+    let passwordValue = document.getElementById("password").value;
+
+    let data = new FormData();
+    data.append("method", "Login");
+    data.append("email", emailValue);
+    data.append("password", passwordValue);
+
+    let xtmlRequest = new XMLHttpRequest();
+    xtmlRequest.open("POST", "Services/Service.php");
+    xtmlRequest.onload = function () {
+        //
+    };
+    xtmlRequest.send(data);
+    console.log('Bidna');
+    return false;
+}
+
+
